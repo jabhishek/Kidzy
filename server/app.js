dcb37c829006077e96c4fa3618d0e92d0f833d5c
Abhishek Jain <abhi2000@gmail.com>
2014-10-03 15:19:58 +0100

    Added index.html file

--- server/app.js
+++ server/app.js
@@ -1,11 +1,23 @@
 var express = require("express");
+var path = require("path");
 var app = express();
 var port = process.env.PORT || 9000;
 
-app.get('/', function(req, res){
-    res.send('Hello World');
-});
+var rootPath = path.normalize(__dirname + '/..');
+var appPath = path.join(rootPath, 'client');
+
+app.use(express.static(appPath));
+app.set("appPath", appPath);
+
+app.route('/*')
+    .get(function(req, res) {
+        res.sendFile(app.get('appPath') + '/index.html');
+    });
+console.log(appPath);
 
 app.listen(port, function() {
     console.log('Listening on port ' +  port + " in mode: " + process.env.NODE_ENV);
 });
+
+// Expose app
+module.exports = app;

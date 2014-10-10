b909d1f86cdec1a64c99eaf8e549b10ebd529c1a
Abhishek Jain <abhi2000@gmail.com>
2014-10-10 11:27:12 +0100

    Added styling to navbar

--- client/app/NavBar/NavBar.directive.js
+++ client/app/NavBar/NavBar.directive.js
@@ -1,19 +1,15 @@
 (function (app) {
-
+    'use strict';
     // todo-abhi - show navbar conditionslly
     app.directive('ajNavbar', function () {
         return {
-            restrict: "EA",
-            //template: "<div class='navbar'>NavBar</div>"
-            templateUrl: "NavBar/NavBar.html",
+            restrict: 'EA',
+            templateUrl: 'NavBar/NavBar.html',
             controller: function() {
-                "use strict";
                 var vm = this;
-                this.isCollapsed = true;
+                vm.isCollapsed = true;
             },
             controllerAs: 'navBarVm'
-        }
+        };
     });
 })(angular.module('HousePointsApp'));
-
-

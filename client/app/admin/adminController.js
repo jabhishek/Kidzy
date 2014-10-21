15ac511c0ec04d56ac1e4770a724f776a8727186
Abhishek Jain <abhi2000@gmail.com>
2014-10-21 16:21:37 +0100

    Added animation to view and usersList

--- client/app/admin/adminController.js
+++ client/app/admin/adminController.js
@@ -1,14 +1,12 @@
 (function (app) {
     'use strict';
-    app.controller('adminController', function (AuthService, UserService) {
+    app.controller('adminController', function (AuthService, UserService, $timeout) {
         var vm = this;
         vm.Auth = AuthService;
         vm.users = [];
 
         UserService.getAllUsers().then(function(data) {
             data.forEach(function(user) {
-                vm.users.push(user);
-                vm.users.push(user);
                 vm.users.push(user);
             })
         });

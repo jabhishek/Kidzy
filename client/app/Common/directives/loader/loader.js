4bd2e65f18186dd35e67158709ce05ff828ef9ce
Abhishek Jain <abhi2000@gmail.com>
2014-11-06 11:03:16 +0000

    add cleanup logic on scope destroy

--- client/app/Common/directives/loader/loader.js
+++ client/app/Common/directives/loader/loader.js
@@ -4,36 +4,44 @@
         return {
             restrict: 'A',
             link: function (scope, elem, attrs, ngModel) {
+                var hideLoaderTimeout;
                 scope.data = {
                     startTime: undefined
                 };
                 var unregisterStart = $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
-                    console.group('$stateChangeStart');
+                    console.group('loader - $stateChangeStart');
                     console.log('from ' + fromState.name + " to " + toState.name);
                     var date = new Date();
                     scope.data.startTime = date;
                     console.log('start time');
                     console.log(scope.data.startTime);
+
                     elem.removeClass('ng-hide');
                     console.groupEnd('$stateChangeStart');
 
                 });
                 var unregisterSuccess = $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-                    console.group('$stateChangeSuccess');
+                    console.group('loader - $stateChangeSuccess');
                     console.log('from ' + fromState.name + " to " + toState.name);
                     console.log('state Transition time: ' + (new Date() - scope.data.startTime) + ' ms');
-                    $timeout(function() {
+                    hideLoaderTimeout = $timeout(function() {
                         elem.addClass('ng-hide');
                     }, 300);
 
                     console.groupEnd('$stateChangeSuccess');
                 });
 
-                $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+                var unregisterError = $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
                     elem.addClass('ng-hide');
                 });
 
-                scope.$on('destroy', [unregisterSuccess, unregisterStart]);
+                scope.$on('destroy', function() {
+                    console.log('unregistering');
+                    unregisterStart();
+                    unregisterSuccess();
+                    unregisterError();
+                    $timeout.cancel(hideLoaderTimeout);
+                });
             }
         };
     });

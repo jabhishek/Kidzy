25af65e86fbb42a37b8ff83583bdab6affc10297
Abhishek Jain <abhi2000@gmail.com>
2014-11-11 12:18:36 +0000

    Added registration unit tests

--- client/app/Register/registerController.spec.js
+++ client/app/Register/registerController.spec.js
@@ -20,12 +20,30 @@ describe("registerController", function () {
         RegisterCtrl = $controller('registerController', {UserService: UserService, $state: $state});
     }));
 
-    beforeEach(function () {
+    function simulateCreateUserPromise (obj) {
         "use strict";
         var deferred = $q.defer();
-        deferred.resolve();
+        if (obj.resolve) {
+            deferred.resolve();
+        } else {
+            deferred.reject();
+        }
+
         spyOn(UserService, 'createUser').and.returnValue(deferred.promise);
-    });
+    }
+
+    function simulateLoginPromise (obj) {
+        "use strict";
+        var deferred = $q.defer();
+        if (obj.resolve) {
+            deferred.resolve();
+        } else {
+            deferred.reject();
+        }
+
+        spyOn(AuthService, 'login').and.returnValue(deferred.promise);
+    }
+
     it('should have user defined', function () {
         expect(RegisterCtrl.user).toBeDefined();
         expect(angular.isObject(RegisterCtrl.user)).toBeTruthy();
@@ -45,24 +63,56 @@ describe("registerController", function () {
     });
 
     it('should call UserService.createUser if form is valid', function () {
+        simulateCreateUserPromise({resolve: true});
         var user = {email: 'a@a.com'};
         RegisterCtrl.submit(true, user);
         expect(UserService.createUser).toHaveBeenCalledWith(user);
     });
 
     it('should not call UserService.createUser if form is invalid', function () {
+        simulateCreateUserPromise({resolve: true});
         RegisterCtrl.submit(false, {});
         expect(UserService.createUser).not.toHaveBeenCalled();
     });
 
-    it('should navigate to main view if successfully registered', function () {
-        spyOn($state, 'go').and.callThrough();
-        spyOn(AuthService, 'login').and.returnValue($q.defer().promise);
+    it('should login the user once successfully registered', function () {
+        simulateCreateUserPromise({resolve: true});
+        simulateLoginPromise({resolve: true});
 
         RegisterCtrl.submit(true, {});
         $rootScope.$digest();
         expect(AuthService.login).toHaveBeenCalled();
-        expect($state.go).toHaveBeenCalledWith('main');
+    });
+
+    it('should not login the user if registration not successful', function () {
+        simulateCreateUserPromise({resolve: false});
+        simulateLoginPromise({resolve: true});
+
+        RegisterCtrl.submit(true, {});
+        $rootScope.$digest();
+        expect(AuthService.login).not.toHaveBeenCalled();
+    });
+
+    it('should navigate to main view if successfully registered', function () {
+        simulateCreateUserPromise({resolve: true});
+        simulateLoginPromise({resolve: true});
+
+        spyOn($state, 'go').and.returnValue($q.defer().promise);
+
+        RegisterCtrl.submit(true, {});
+        $rootScope.$digest();
+        expect($state.go).toHaveBeenCalled();
+    });
+
+    it('should not navigate to main view if not successfully registered', function () {
+        simulateCreateUserPromise({resolve: true});
+        simulateLoginPromise({resolve: false});
+
+        spyOn($state, 'go').and.returnValue($q.defer().promise);
+
+        RegisterCtrl.submit(false, {});
+        $rootScope.$digest();
+        expect($state.go).not.toHaveBeenCalled();
     });
 });
 

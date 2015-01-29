2cff46652349614d1f253d06c4c672e4fb48831f
Abhishek Jain <abhi2000@gmail.com>
2015-01-29 14:10:33 +0000

    Missing files

--- client/app/main/childView/childView.directive.js
+++
@@ -1,9 +0,0 @@
-(function (app) {
-    'use strict';
-    app.directive('childView', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'main/childView/childView.html'
-        };
-    });
-})(angular.module('HousePointsApp'));
